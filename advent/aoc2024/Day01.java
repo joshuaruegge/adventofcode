50832a2c38a551d5b290316098b8d2d43a479606
joshuaruegge <31775138+joshuaruegge@users.noreply.github.com>
2024-11-30 21:01:45 -0700

    2024 prep

---
+++ advent/aoc2024/Day01.java
@@ -0,0 +1,25 @@
+package advent.aoc2024;
+
+import advent.utilities.general.DayRunner;
+import advent.utilities.general.IDay;
+import advent.utilities.general.Input;
+
+public class Day01 implements IDay {
+
+    static String input;
+
+    public static void main(String[] args) {
+        input = Input.fetchInput(24, 1);
+        DayRunner.run(new Day01());
+    }
+
+    @Override
+    public String part1() {
+        return null;
+    }
+
+    @Override
+    public String part2() {
+        return null;
+    }
+}
