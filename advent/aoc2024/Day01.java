f871f2e82301d3086ce3262fec7a968cf144605a
joshuaruegge <31775138+joshuaruegge@users.noreply.github.com>
2024-11-30 22:10:19 -0700

    2024 Day 1

--- advent/aoc2024/Day01.java
+++ advent/aoc2024/Day01.java
@@ -4,22 +4,60 @@ import advent.utilities.general.DayRunner;
 import advent.utilities.general.IDay;
 import advent.utilities.general.Input;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class Day01 implements IDay {
 
     static String input;
 
     public static void main(String[] args) {
-        input = Input.fetchInput(24, 1);
+        input = Input.fetchInput(2024, 1);
         DayRunner.run(new Day01());
     }
 
     @Override
     public String part1() {
-        return null;
+        int result = 0;
+
+        ArrayList<Integer> firstList = new ArrayList<>();
+        ArrayList<Integer> secondList = new ArrayList<>();
+
+        for(String s : input.split("\n")) {
+            String[] chunks = s.split("   ");
+            firstList.add(Integer.parseInt(chunks[0]));
+            secondList.add(Integer.parseInt(chunks[1]));
+        }
+
+        Collections.sort(firstList);
+        Collections.sort(secondList);
+
+        for(int i = 0; i < firstList.size(); i++) {
+            result += Math.abs(secondList.get(i) - firstList.get(i));
+        }
+
+        return Integer.toString(result);
     }
 
     @Override
     public String part2() {
-        return null;
+        int result = 0;
+
+        ArrayList<Integer> firstList = new ArrayList<>();
+        HashMap<Integer, Integer> secondListFreqs = new HashMap<>();
+
+        for(String s : input.split("\n")) {
+            String[] chunks = s.split("   ");
+            firstList.add(Integer.parseInt(chunks[0]));
+            int secondListItem = Integer.parseInt(chunks[1]);
+            secondListFreqs.put(secondListItem, secondListFreqs.getOrDefault(secondListItem,0) + 1);
+        }
+
+        for(int i : firstList) {
+            result += i * secondListFreqs.getOrDefault(i, 0);
+        }
+
+        return Integer.toString(result);
     }
 }
